8c1a3a8c658f736892e3137f6ad2d19aa8fa10c4
Noah Lin <nglin34@gmail.com>
2026-02-13 23:01:07 -0500

    Initial Commit

---
+++ BrainDump/BrainDumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainDumpApp.swift
+//  BrainDump
+//
+//  Created by Noah Lin  on 2026-02-13.
+//
+
+import SwiftUI
+
+@main
+struct BrainDumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrainDump/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrainDump
+//
+//  Created by Noah Lin  on 2026-02-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
